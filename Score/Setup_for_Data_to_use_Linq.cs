a429041d11f3b1f1832d67901cebde9c868437da
JMicahMan <j.micah.manzanares@gmail.com>
2019-03-11 17:43:35 -0500

    Add project files.

---
+++ Score/Setup_for_Data_to_use_Linq.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Score;
+
+
+namespace GuidedExercise_05
+{
+    public partial class Form1 : Form
+    {
+
+        public static string Filenamecsv = "datadoit.csv";
+        public List<Score> PlayerScores = new List<Score>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var playerdata = File.ReadAllLines(Filenamecsv);
+
+
+            foreach (var line in playerdata)
+            {
+                var entry = line.Split(',');
+                var score = new Score
+                {
+                    levelName = entry[0],
+                    playerName = entry[1],
+                    score = float.Parse(entry[2]),
+                    levelTime = float.Parse(entry[3]),
+                    enemiesKilled = (int) float.Parse(entry[4]),
+                    levelCompleted = float.Parse(entry[5]) > 0 
+                };
+
+                PlayerScores.Add(score);
+
+                playerInfoTextBox.Text += line + Environment.NewLine;
+            }
+        }
+    }
+}
